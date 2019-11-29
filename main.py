38ddcc0f6965138492d8df88f033cb0ac5a706ea
Chris Ullyott <contact@chrisullyott.com>
2019-11-29 14:50:59 -0800

    Update main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 from app import *
 
 # Parse arguments
-parser = argparse.ArgumentParser(description='Forecast command line')
+parser = argparse.ArgumentParser(description='wordsearch-solver command line')
 parser.add_argument('puzzle', help='The puzzle file', type=str)
 parser.add_argument('wordlist', help='A wordlist to search', type=str)
 args = parser.parse_args()
@@ -11,6 +11,5 @@ args = parser.parse_args()
 puzzle = Puzzle(args.puzzle)
 solver = Solver(puzzle)
 coords = solver.find_from_wordlist(args.wordlist)
-puzzle.print_all()
 puzzle.print_coords(coords)
 
