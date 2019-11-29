fad76c2e988ff3b4775e15ab5ea81642b7c11a36
Chris Ullyott <contact@chrisullyott.com>
2019-11-29 15:15:21 -0800

    Update main

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import argparse
 from app import *
 
 # Parse arguments
-parser = argparse.ArgumentParser(description='wordsearch-solver command line')
+parser = argparse.ArgumentParser(description='**WORDSEARCH SOLVER COMMAND LINE**')
 parser.add_argument('puzzle', help='The puzzle file', type=str)
-parser.add_argument('wordlist', help='A wordlist to search', type=str)
+parser.add_argument('wordlist', help='A wordlist file for searching', type=str)
 args = parser.parse_args()
 
 puzzle = Puzzle(args.puzzle)
