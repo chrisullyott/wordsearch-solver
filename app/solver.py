58f8716362283a4644caed39e584889ca953c980
Chris Ullyott <contact@chrisullyott.com>
2019-11-29 15:15:39 -0800

    New method #coord_exists

--- app/solver.py
+++ app/solver.py
@@ -16,6 +16,9 @@ class Solver:
     def __init__(self, puzzle):
         self.puzzle = puzzle
 
+    def coord_exists(self, row_key, col_key):
+        return (row_key < self.puzzle.dim[0]) and (col_key < self.puzzle.dim[1])
+
     def find_candidates(self, char):
         candidates = []
         for row_key, row in enumerate(self.puzzle.grid):
@@ -26,16 +29,14 @@ class Solver:
 
     def find_word(self, word):
         found_coords = []
-        word = word.strip().upper()
-        word_chars = list(word)
-        dim = self.puzzle.dim
+        word_chars = list(word.strip().upper())
         for candidate in self.find_candidates(word_chars[0]):
             for direction in self.directions:
                 test_coords = []
                 for char_key, char in enumerate(word_chars):
                     row_key = candidate[0] + (direction[0] * char_key)
                     col_key = candidate[1] + (direction[1] * char_key)
-                    coord_exists = row_key < dim[0] and col_key < dim[1]
+                    coord_exists = self.coord_exists(row_key, col_key)
                     if coord_exists and char == self.puzzle.grid[row_key][col_key]:
                         test_coords.append([row_key, col_key])
                     else:
